1ae43a4a1c141a98ce2807786eb80727899cf10a
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-27 22:10:59 -0300

    cleark authenticaion

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <>
-      <div className="mx-5 mt-5 text-lg font-medium">login (unProtected)</div>
-    </>
-  );
-}
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function HomePage() {
   return (
     <>
-      <div className="mx-5 mt-5 text-lg font-medium">home (protected)</div>
+      <div className="mx-5 mt-5 text-lg font-medium">
+        home (protected)
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </>
   );
 }
